a1dfc3552878c39149d24f933804923fefcab221
YuJiAmc <1002273469@qq.com>
2020-10-28 11:02:15 +0800

    commit feat: 代码优化

--- js/common.js
+++ js/common.js
@@ -3,6 +3,7 @@ $(function(){
   $("#head").load("./header.html?v="+new Date().getTime());
   $("#foot").load("./footer.html?v="+new Date().getTime());
   $("#contact").load("./contact.html?v="+new Date().getTime());
+  // 置顶
   setTimeout(() => {
       $(window).scroll(function () {
           if ($(window).scrollTop() > $("#banner").height()){
@@ -14,4 +15,21 @@ $(function(){
           }
       })
   }, 16.7);
+  // 替换img
+  var test = /_min\./
+  $(".compress_img").each(function(){	
+    var reSrc = $(this).attr("src").replace(test,".");
+    $(this).attr("src",reSrc)
+  })
+  // 替换背景图
+  $(".compress_bgi").each(function(){
+    var img=new Image();
+    var bgi = $(this).css("background-image");
+    var url = bgi.slice(bgi.search("/img"),-2).replace(test,".")
+    img.src= "."+url+"";
+    var that = this;
+    img.onload=function(){
+      $(that).css("background-image","url('."+url+"')")
+    };
+  })
 });
